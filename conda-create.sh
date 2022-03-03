0f2629720d7db9115ef99378ebe46a059561aa61
Kevin Horecka <kevin.horecka@gmail.com>
2022-03-03 14:40:02 -0600

    Update conda-create.sh

--- conda-create.sh
+++ conda-create.sh
@@ -2,6 +2,6 @@ echo What do you want to name the environment?
 read envname
 conda deactivate
 conda create -n $envname python==3.9 numpy scipy pandas seaborn scikit-learn jupyter --yes
+conda activate $envname
 conda install bertopic --channel conda-forge --yes
 pip install jupyterlab
-conda activate $envname
