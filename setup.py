ac0572d0812dd829a190941fb997e9cda2e3bd95
Kevin Horecka <kevin.horecka@gmail.com>
2022-03-02 19:11:30 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='kevroy314-conda-base',
+      version='0.1',
+      description='kevroy314 preferred base conda environment',
+      url='https://github.com/kevroy314/conda-base/',
+      author='Kevin Horecka',
+      author_email='kevin.horecka@gmail.com',
+      license='MIT',
+      packages=[],
+      zip_safe=False)
